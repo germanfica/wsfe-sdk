53cb5ea60a7fcc016e117e45fc9bf911b558cdae
German Fica <germafica@gmail.com>
2025-01-17 15:22:05 -0300

    Add BaseAddress

---
+++ src/main/java/com/germanfica/wsfe/net/BaseAddress.java
@@ -0,0 +1,9 @@
+package com.germanfica.wsfe.net;
+
+/** The base address to use for the request. */
+public enum BaseAddress {
+    /** https://wsaa.afip.gov.ar */
+    WSAA,
+    /** https://servicios1.afip.gov.ar */
+    WSFE;
+}
