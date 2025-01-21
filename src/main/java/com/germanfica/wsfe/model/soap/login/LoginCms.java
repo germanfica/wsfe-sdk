113f02fea1e8c4e9cdee8ba35c82d47f872e241f
German Fica <germafica@gmail.com>
2025-01-20 22:37:39 -0300

    Refactor login models

--- src/main/java/com/germanfica/wsfe/model/soap/login/LoginCms.java
+++ src/main/java/com/germanfica/wsfe/model/soap/login/LoginCms.java
@@ -1,22 +1,17 @@
 package com.germanfica.wsfe.model.soap.login;
 
 import jakarta.xml.bind.annotation.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "", propOrder = {
     "in0"
 })
 @XmlRootElement(name = "loginCms")
 public class LoginCms {
-
     @XmlElement(required = true)
     protected String in0;
-
-    public String getIn0() {
-        return in0;
-    }
-
-    public void setIn0(String value) {
-        this.in0 = value;
-    }
 }
