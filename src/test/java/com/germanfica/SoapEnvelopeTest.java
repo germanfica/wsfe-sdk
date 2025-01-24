162b64270a2fe06407cd028cb9b2225064fe11c4
German Fica <germafica@gmail.com>
2025-01-24 00:50:07 -0300

    Add new tests

--- src/test/java/com/germanfica/SoapEnvelopeTest.java
+++ src/test/java/com/germanfica/SoapEnvelopeTest.java
@@ -9,14 +9,13 @@ import com.germanfica.wsfe.model.soap.envelope.SoapFault;
 import com.germanfica.wsfe.model.soap.loginticket.CredentialsType;
 import com.germanfica.wsfe.model.soap.loginticket.HeaderType;
 import com.germanfica.wsfe.model.soap.loginticket.LoginTicketResponseType;
+import com.germanfica.wsfe.utils.ArcaDateTimeUtils;
 import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.JAXBException;
 import jakarta.xml.bind.Marshaller;
 import jakarta.xml.bind.Unmarshaller;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-
 import javax.xml.datatype.DatatypeConfigurationException;
 import java.io.StringReader;
 import java.io.StringWriter;
@@ -24,6 +23,127 @@ import java.io.StringWriter;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SoapEnvelopeTest {
+
+    @Test
+    void testConvertXmlToObjectLoginTicketResponseType() throws Exception {
+        String xmlResponse =
+                """
+                <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+                <loginTicketResponse version="1.0">
+                    <header>
+                        <source>CN=wsaahomo, O=AFIP, C=AR, SERIALNUMBER=CUIT 33693450239</source>
+                        <destination>SERIALNUMBER=CUIT xxxxxxxxxxx, CN=test</destination>
+                        <uniqueId>2610407892</uniqueId>
+                        <generationTime>2025-01-21T18:36:40.770-03:00</generationTime>
+                        <expirationTime>2025-01-22T06:36:40.770-03:00</expirationTime>
+                    </header>
+                    <credentials>
+                        <token>xxxxxxxxxxxxxxxxxxxxxxxxx</token>
+                        <sign>xxxxxxxxxxxxxxxxxxxxxxxxx</sign>
+                    </credentials>
+                </loginTicketResponse>
+                """;
+
+        LoginTicketResponseType loginTicketResponse = convertXmlToObjectWithUnmarshaller(xmlResponse, LoginTicketResponseType.class);
+
+        assertEquals("CN=wsaahomo, O=AFIP, C=AR, SERIALNUMBER=CUIT 33693450239", loginTicketResponse.getHeader().getSource());
+        assertEquals("SERIALNUMBER=CUIT xxxxxxxxxxx, CN=test", loginTicketResponse.getHeader().getDestination());
+        assertEquals("2610407892", loginTicketResponse.getHeader().getUniqueId());
+        assertEquals("2025-01-21T18:36:40.770-03:00", ArcaDateTimeUtils.formatDateTime(loginTicketResponse.getHeader().getGenerationTime(), ArcaDateTimeUtils.DateTimeFormat.ISO_8601_FULL, ArcaDateTimeUtils.TimeZoneOffset.ARGENTINA));
+        assertEquals("2025-01-22T06:36:40.770-03:00", ArcaDateTimeUtils.formatDateTime(loginTicketResponse.getHeader().getExpirationTime(), ArcaDateTimeUtils.DateTimeFormat.ISO_8601_FULL, ArcaDateTimeUtils.TimeZoneOffset.ARGENTINA));
+        assertEquals("xxxxxxxxxxxxxxxxxxxxxxxxx", loginTicketResponse.getCredentials().getToken());
+        assertEquals("xxxxxxxxxxxxxxxxxxxxxxxxx", loginTicketResponse.getCredentials().getSign());
+    }
+
+    @Test
+    void testConvertXmlToObjectLoginTicketResponseTypeWithSoapEnvelope() throws Exception {
+        String xmlResponse =
+                """
+                <?xml version="1.0" encoding="utf-8"?><soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"><soapenv:Body><loginCmsResponse xmlns="http://wsaa.view.sua.dvadac.desein.afip.gov.ar/"><loginCmsReturn><?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+                <loginTicketResponse version="1.0">
+                    <header>
+                        <source>CN=wsaahomo, O=AFIP, C=AR, SERIALNUMBER=CUIT 33693450239</source>
+                        <destination>SERIALNUMBER=CUIT xxxxxxxxxxx, CN=test</destination>
+                        <uniqueId>283060288</uniqueId>
+                        <generationTime>2025-01-22T00:23:57.202-03:00</generationTime>
+                        <expirationTime>2025-01-22T12:23:57.202-03:00</expirationTime>
+                    </header>
+                    <credentials>
+                        <token>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</token>
+                        <sign>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</sign>
+                    </credentials>
+                </loginTicketResponse>
+                </loginCmsReturn></loginCmsResponse></soapenv:Body></soapenv:Envelope>
+                """;
+
+        LoginTicketResponseType loginTicketResponse = convertXmlToObject(xmlResponse, LoginTicketResponseType.class);
+
+        assertEquals("CN=wsaahomo, O=AFIP, C=AR, SERIALNUMBER=CUIT 33693450239", loginTicketResponse.getHeader().getSource());
+        assertEquals("SERIALNUMBER=CUIT xxxxxxxxxxx, CN=test", loginTicketResponse.getHeader().getDestination());
+        assertEquals("2610407892", loginTicketResponse.getHeader().getUniqueId());
+        assertEquals("2025-01-21T18:36:40.770-03:00", ArcaDateTimeUtils.formatDateTime(loginTicketResponse.getHeader().getGenerationTime(), ArcaDateTimeUtils.DateTimeFormat.ISO_8601_FULL, ArcaDateTimeUtils.TimeZoneOffset.ARGENTINA));
+        assertEquals("2025-01-22T06:36:40.770-03:00", ArcaDateTimeUtils.formatDateTime(loginTicketResponse.getHeader().getExpirationTime(), ArcaDateTimeUtils.DateTimeFormat.ISO_8601_FULL, ArcaDateTimeUtils.TimeZoneOffset.ARGENTINA));
+        assertEquals("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", loginTicketResponse.getCredentials().getToken());
+        assertEquals("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", loginTicketResponse.getCredentials().getSign());
+    }
+
+    @Test
+    void testConvertXmlToObjectLoginTicketResponseTypeWithNoSoapEnvelope() throws Exception {
+        String xmlResponse =
+                """
+                <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+                <loginTicketResponse version="1.0">
+                    <header>
+                        <source>CN=wsaahomo, O=AFIP, C=AR, SERIALNUMBER=CUIT 33693450239</source>
+                        <destination>SERIALNUMBER=CUIT xxxxxxxxxxx, CN=test</destination>
+                        <uniqueId>283060288</uniqueId>
+                        <generationTime>2025-01-22T00:23:57.202-03:00</generationTime>
+                        <expirationTime>2025-01-22T12:23:57.202-03:00</expirationTime>
+                    </header>
+                    <credentials>
+                        <token>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</token>
+                        <sign>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</sign>
+                    </credentials>
+                </loginTicketResponse>
+                </loginCmsReturn>
+                """;
+
+        LoginTicketResponseType loginTicketResponse = convertXmlToObject(xmlResponse, LoginTicketResponseType.class);
+
+        assertEquals("CN=wsaahomo, O=AFIP, C=AR, SERIALNUMBER=CUIT 33693450239", loginTicketResponse.getHeader().getSource());
+        assertEquals("SERIALNUMBER=CUIT xxxxxxxxxxx, CN=test", loginTicketResponse.getHeader().getDestination());
+        assertEquals("283060288", loginTicketResponse.getHeader().getUniqueId());
+        assertEquals("2025-01-22T00:23:57.202-03:00", ArcaDateTimeUtils.formatDateTime(loginTicketResponse.getHeader().getGenerationTime(), ArcaDateTimeUtils.DateTimeFormat.ISO_8601_FULL, ArcaDateTimeUtils.TimeZoneOffset.ARGENTINA));
+        assertEquals("2025-01-22T12:23:57.202-03:00", ArcaDateTimeUtils.formatDateTime(loginTicketResponse.getHeader().getExpirationTime(), ArcaDateTimeUtils.DateTimeFormat.ISO_8601_FULL, ArcaDateTimeUtils.TimeZoneOffset.ARGENTINA));
+        assertEquals("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", loginTicketResponse.getCredentials().getToken());
+        assertEquals("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", loginTicketResponse.getCredentials().getSign());
+    }
+
+    @Test
+    void testConvertXmlToObjectSoapEnvelope() throws Exception {
+        String xmlResponse =
+                """
+                <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">
+                   <soapenv:Body>
+                      <soapenv:Fault>
+                         <faultcode xmlns:ns1="http://xml.apache.org/axis/">ns1:coe.alreadyAuthenticated</faultcode>
+                         <faultstring>El CEE ya posee un TA valido para el acceso al WSN solicitado</faultstring>
+                         <detail>
+                            <ns2:exceptionName xmlns:ns2="http://xml.apache.org/axis/">gov.afip.desein.dvadac.sua.view.wsaa.LoginFault</ns2:exceptionName>
+                            <ns3:hostname xmlns:ns3="http://xml.apache.org/axis/">wsaaext0.homo.afip.gov.ar</ns3:hostname>
+                         </detail>
+                      </soapenv:Fault>
+                   </soapenv:Body>
+                </soapenv:Envelope>
+                """;
+
+        SoapEnvelope envelope = convertXmlToObject(xmlResponse, SoapEnvelope.class);
+
+        assertEquals("ns1:coe.alreadyAuthenticated", envelope.getBody().getFault().getFaultCode());
+        assertEquals("El CEE ya posee un TA valido para el acceso al WSN solicitado", envelope.getBody().getFault().getFaultString());
+        assertEquals("gov.afip.desein.dvadac.sua.view.wsaa.LoginFault", envelope.getBody().getFault().getDetail().getExceptionName());
+        assertEquals("wsaaext0.homo.afip.gov.ar", envelope.getBody().getFault().getDetail().getHostname());
+    }
 
     @Test
     void testUnmarshalLoginTicketResponseType() throws JAXBException {
@@ -215,6 +335,24 @@ public class SoapEnvelopeTest {
         }
     }
 
+    /**
+     * Converts an XML string to an object of the specified type using Unmarshaller.
+     *
+     * @param xmlString the XML string to convert
+     * @param clazz     the class type of the object
+     * @param <T>       the type parameter
+     * @return the deserialized object of type T
+     * @throws XmlMappingException if an error occurs during deserialization
+     */
+    private static <T> T convertXmlToObjectWithUnmarshaller(String xmlString, Class<T> clazz) throws XmlMappingException {
+        try {
+            JAXBContext jaxbContext = JAXBContext.newInstance(clazz);
+            var unmarshaller = jaxbContext.createUnmarshaller();
+            return (T) unmarshaller.unmarshal(new StringReader(xmlString));
+        } catch (JAXBException e) {
+            throw new XmlMappingException("Error mapping XML to object with Unmarshaller", xmlString, e);
+        }
+    }
 
      /**
      * Converts an XML string to an object of the specified type.
