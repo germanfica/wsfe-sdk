61d878ea45e1710f4798b520e13aa529e2d944a0
German Fica <germafica@gmail.com>
2025-04-27 14:25:04 -0300

    docs: add TODOs for replacing ConfigManager with ProviderChain

--- src/main/java/com/germanfica/wsfe/util/ConfigManager.java
+++ src/main/java/com/germanfica/wsfe/util/ConfigManager.java
@@ -2,6 +2,21 @@ package com.germanfica.wsfe.util;
 
 import java.io.*;
 import java.util.Properties;
+//TODO 1: Reemplazar este ConfigManager por SystemSetting, SystemSettingUtils y SystemSettingUtilsTestBackdoor. Ya que permite, System.getenv() y System.getProperty() <3
+// https://github.com/aws/aws-sdk-java-v2/blob/master/utils/src/main/java/software/amazon/awssdk/utils/SystemSetting.java
+// https://github.com/aws/aws-sdk-java-v2/blob/master/utils/src/main/java/software/amazon/awssdk/utils/internal/SystemSettingUtils.java
+// https://github.com/aws/aws-sdk-java-v2/blob/master/utils/src/main/java/software/amazon/awssdk/utils/internal/SystemSettingUtilsTestBackdoor.java
+
+//TODO 2: Agregar el Provider Chain <3 (DefaultCredentialsProvider, algo similar a ProfileCredentialsProvider...
+// https://github.com/aws/aws-sdk-java-v2/blob/2.31.30/core/auth/src/main/java/software/amazon/awssdk/auth/credentials/DefaultCredentialsProvider.java
+// https://github.com/aws/aws-sdk-java-v2/blob/2.31.30/core/auth/src/main/java/software/amazon/awssdk/auth/credentials/ProfileCredentialsProvider.java
+// https://github.com/aws/aws-sdk-java-v2/blob/2.31.30/core/profiles/src/main/java/software/amazon/awssdk/profiles/ProfileFile.java // No hace falta. No es necesario tener perfiles, De hecho, debería ser un "único perfil"... como el SDK de stripe con su apiKey
+
+//TODO 3: la idea es permitir ingresar un signedCmsBase64 y también firmar un cms desde dado el certificado como archivo.
+// Nota: recordar que
+// - signedCmsBase64: tiene un vencimiento de 2 años (dado que este, se genera a partir del certificado generado en ARCA). Tiene que ser generado manualmente (no lo porporciona ARCA).
+// - Token y sign (TA, Ticket de Acceso): tiene un periodo de vencimiento más corto de 12 horas desde la fecha de emisión (generado por WsfeClient y usado por WsfeClient)
+// Docs: https://www.afip.gob.ar/ws/WSAA/Especificacion_Tecnica_WSAA_1.2.2.pdf
 
 /**
  * Maneja la lectura y escritura del archivo ~/.wsfe/config.properties,
